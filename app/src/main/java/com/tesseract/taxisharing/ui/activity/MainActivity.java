15378b3d16fcb8e363fdade8ebcf56dbaffa46fb
HamudiHasan <hhsonet@gmail.com>
2016-09-09 00:08:11 +0600

    Create Package

--- app/src/main/java/com/tesseract/taxisharing/ui/activity/MainActivity.java
+++ app/src/main/java/com/tesseract/taxisharing/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tesseract.taxisharing;
+package com.tesseract.taxisharing.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tesseract.taxisharing.R;
 
 public class MainActivity extends AppCompatActivity {
 
