be420b33a981c6ef6fc302a517a10c6bfaa0021b
HamudiHasan <hhsonet@gmail.com>
2016-09-09 01:51:05 +0600

    Drawer added

--- app/src/main/java/com/tesseract/taxisharing/ui/activity/MainActivity.java
+++ app/src/main/java/com/tesseract/taxisharing/ui/activity/MainActivity.java
@@ -1,8 +1,18 @@
 package com.tesseract.taxisharing.ui.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.tesseract.taxisharing.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Drawer();
+    }
+    public void Drawer() {
+        final Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withDrawerWidthDp(200)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Navigation"),
+                        new PrimaryDrawerItem().withName("LogOut").withSetSelected(true).withIdentifier(1)
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem.equals(1)) {
+                            startActivity(new Intent(MainActivity.this,MapsActivity.class));
+                            Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                        }
+
+                        return true;
+                    }
+                }).withDrawerGravity(Gravity.LEFT)
+                .build();
+        result.openDrawer();
+        result.closeDrawer();
+        result.isDrawerOpen();
     }
 }
--- app/src/main/java/com/tesseract/taxisharing/ui/activity/MapsActivity.java
+++ app/src/main/java/com/tesseract/taxisharing/ui/activity/MapsActivity.java
@@ -1,19 +1,24 @@
 package com.tesseract.taxisharing.ui.activity;
 
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.tesseract.taxisharing.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    View snackbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
@@ -43,5 +50,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                return false;
+            }
+        });
     }
 }
