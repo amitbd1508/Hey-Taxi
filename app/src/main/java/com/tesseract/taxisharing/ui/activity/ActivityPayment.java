5a7e02c84b746bf2d97effcd2a10c2f83634503c
HamudiHasan <hhsonet@gmail.com>
2016-09-16 23:33:10 +0600

    Drawer item and activity added

---
+++ app/src/main/java/com/tesseract/taxisharing/ui/activity/ActivityPayment.java
@@ -0,0 +1,15 @@
+package com.tesseract.taxisharing.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tesseract.taxisharing.R;
+
+public class ActivityPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
